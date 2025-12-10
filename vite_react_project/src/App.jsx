a12a3998ce6a7c74aee5db7d544321babed2ecc6
sani816 <saniakhatun622@gmail.com>
2025-12-10 23:10:14 +0530

    react_projects

---
+++ vite_react_project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FirstComponent from './FirstComponent'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <FirstComponent></FirstComponent>
+        </div>
+        </>
+  )
+}
+
+export default App
---
+++ vite_react_project/src/FirstComponent.jsx
@@ -0,0 +1,4 @@
+function FirstComponent(){
+    return <h2>This is my first React component</h2>
+}
+export default FirstComponent;
