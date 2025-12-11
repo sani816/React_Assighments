48f9e2396c2d9682eeb9d85b8ac885efe6f483ce
sani816 <saniakhatun622@gmail.com>
2025-12-11 21:58:40 +0530

    react_q4

---
+++ react_vite_q4/src/UserProfile.jsx
@@ -0,0 +1,8 @@
+function UserProfile(props){
+   return <div>
+        <h1>Name:{props.name}</h1>
+         <h2>Age:{props.age}</h2>
+        </div>
+   
+}
+export default UserProfile;
---
+++ react_vite_q4/src/Userinfo.jsx
@@ -0,0 +1,11 @@
+import UserProfile from "./UserProfile.jsx";
+
+
+function UserInfo(){
+    return(
+        <div>
+            <UserProfile name="sania" age={20}></UserProfile>
+        </div>
+    )
+}
+export default UserInfo;
