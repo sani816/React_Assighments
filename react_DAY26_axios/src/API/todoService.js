34c8d6f24b0c8737308c2e93812cbc8ec559d4a8
sani816 <saniakhatun622@gmail.com>
2025-12-28 13:00:53 +0530

    Add files via upload

--- react_DAY26_axios/src/API/todoService.js
+++ react_DAY26_axios/src/API/todoService.js
@@ -1,1 +1,11 @@
+import axiosInstance from "./AxiosInstance";
 
+export const getTodos = async () => {
+  const response = await axiosInstance.get("/todos");
+  return response.data;
+};
+
+export const getTodoById = async (id) => {
+  const response = await axiosInstance.get(`/todos/${id}`);
+  return response.data;
+};
