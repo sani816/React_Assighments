e3361429da31bfb6e13be1e9c4ccda9b73b79b2b
sani816 <saniakhatun622@gmail.com>
2026-01-04 19:37:29 +0530

    Add files via upload

--- DAY30_ShadCN/src/pages/Home.jsx
+++ DAY30_ShadCN/src/pages/Home.jsx
@@ -1,1 +1,14 @@
+import FeedbackForm from "../components/apps/FeedbackForm"
+import SlideshowApp from "../components/apps/SlideshowApp"
+import TodoApp from "../components/apps/TodoApp"
 
+
+export default function Home() {
+  return (
+    <div className="space-y-16 p-10">
+      <FeedbackForm />
+      <SlideshowApp />
+      <TodoApp />
+    </div>
+  )
+}
